34648a0975fff3df19cf35df6eb1bd3123a10888
Fabian Vilers <fabian.vilers@dev-one.com>
2018-11-26 17:47:02 +0100

    Add tableaux.js

---
+++ tableaux.js
@@ -0,0 +1,23 @@
+// Sort
+[8, 10, 1, 5].sort();
+
+// Addition
+[1, 2, 3] + [4, 5, 6];
+
+// Multiplication
+[4] * [4];
+
+// Is it really empty?
+let arr = [,];
+arr.length;
+arr.indexOf(arr[0]);
+
+// 3 in, 2 out
+[, , ,].join();
+
+// Real length?
+const array5 = [1, 2, 3, 4, 5, 6, 7, 8];
+array5.length;
+array5[-1] = 'Hey Dev Day';
+array5.length;
+array5[array5.indexOf(99)];
