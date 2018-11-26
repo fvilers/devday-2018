83e9ff2f6fd9a31fc4e82f3647eb9da612cbd0ba
Fabian Vilers <fabian.vilers@dev-one.com>
2018-11-26 17:45:08 +0100

    Add vrai-ou-faux.js

---
+++ vrai-ou-faux.js
@@ -0,0 +1,25 @@
+// Boolean
+if (true) {
+  ('Hello Dev Day!');
+}
+
+// String
+const event = 'Dev Day';
+if (event) {
+  'Hello ' + event + '!';
+}
+
+// Number
+const year = 2018;
+if (year) {
+  'Hello ' + event + ' ' + year + '!';
+}
+
+// Object
+const now = new Date();
+if (now) {
+  '[' + now + '] Hello ' + event + ' ' + year + '!';
+}
+
+// Obviously!
+3 > 2 > 1;
